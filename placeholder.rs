45e170926663a7cf35b85bfad7a98474c63bdcb6
leftstack <leftstack@proton.me>
2026-09-04 12:08:00 +0000

    Add files via upload

---
+++ placeholder.rs
@@ -0,0 +1,8 @@
+// This file exists solely to identify this repository's primary
+// language on GitHub (via Linguist), since compiled binaries alone
+// don't carry any language metadata.
+//
+// It is not part of the actual build and is not linked into any
+// distributed binary.
+
+fn main() {}
